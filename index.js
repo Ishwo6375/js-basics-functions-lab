90982ee7916c609ebc3fccbf58700fe2efe32b25
ishwo6375 <ishaan.prazapati6375@gmail.com>
2021-04-02 22:41:29 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
+
+  
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(blocks) {
+    let headQuarters = 42;
+    let deliveryLocation;
+    if (blocks > headQuarters) {
+        deliveryLocation = blocks - headQuarters;
+      } else {
+        deliveryLocation = headQuarters - blocks;
+        }
+    return deliveryLocation;
+}
+
+function distanceFromHqInFeet(blocks) {
+    return distanceFromHqInBlocks(blocks) * 264;
+}
+
+function distanceTravelledInFeet(startingBlock,endingBlock) {
+    if (startingBlock < endingBlock) {
+        return (endingBlock - startingBlock) * 264;
+    } else {
+        return (startingBlock - endingBlock) * 264;
+    }
+}
+
+function calculatesFarePrice(startingBlock,endingBlock) {
+    let distance = distanceTravelledInFeet(startingBlock,endingBlock);
+    if (distance <= 400) {
+      return 0
+    } else if (distance > 400 && distance <= 2000) {
+        return .02 * (distance - 400);
+      } else if (distance > 2000 && distance < 2500) {
+        return 25;
+      } else {
+        return 'cannot travel that far';
+      }
+}
